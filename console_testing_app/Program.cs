47bf90c1b6f82a47e1ee77f9cfb29928ed33da27
Túlio Paim <tulio.paim@mw.far.br>
2020-04-03 18:30:43 -0300

    Add project files.

---
+++ console_testing_app/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace console_testing_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SpinWords("Stop Spinning My Words"));
+            Console.WriteLine(SpinWords("Hey fellow warriors"));
+            Console.WriteLine(SpinWords("This is a Test"));
+            Console.WriteLine(SpinWords("This is another Test"));
+            Console.ReadLine();
+        }
+
+        public bool VerificaSeHaLetraRepetida(string entrada) => entrada.ToLower().Distinct().Count() < entrada.Length;
+
+        public static int Persistence(int num)
+        {
+            var count = 0;
+            while (num.ToString().Length > 1)
+            { 
+                var numeros = num.ToString().Select(num => int.Parse(num.ToString())).ToList();
+                num = numeros[0];
+                for (var i = 1; i < numeros.Count; i++) num *= numeros[i];
+                count++;
+            }
+
+            return count;
+        }
+
+        public static string ToCamelCase(string phrase)
+        {
+            var camel = "";
+
+            for (var i = 0; i < phrase.Length; i++)
+            {
+                camel += (phrase[i] != '_' && phrase[i] != '-')
+                        ? phrase[i].ToString()
+                        : (i == phrase.Length - 1)
+                            ? ""
+                            : phrase[++i].ToString().ToUpper();
+            }
+
+            return camel.Trim();
+        }
+
+        public static string ToCamelCase2(string phrase)
+        {
+            string upperFirstLetter(List<string> words)
+            {
+                var final = words[0]; //sempre insere a primeira como estiver
+                for (var i = 1; i < words.Count(); i++) //itera a partir da segunda
+                    final += !string.IsNullOrEmpty(words[i])
+                        ? char.ToUpper(words[i][0]) + words[i]?.Substring(1) //primeira letra maiuscula + resto da palavra
+                        : "";
+
+                return final;
+            }
+
+            var words1 = phrase.Split("-").ToList();
+            var final1 = upperFirstLetter(words1);
+            var words2 = final1.Split("_").ToList();
+            return upperFirstLetter(words2);
+        }
+
+        public static string SpinWords(string phrase)
+        {
+            return string.Join(" ", phrase.Split(' ').Select(w => w.Length < 5 ? w : new string(w.Reverse().ToArray())).ToList());
+        }
+
+        public static int IntegerDiference(List<int> lista, int dif)
+        {
+            lista.Sort();
+            var count = 0;
+            for (var i = 0; i < lista.Count(); i++)
+                for (var j = i + 1; j < lista.Count(); j++)
+                    if (lista[j] - lista[i] == dif)
+                        count++;
+
+            return count;
+        }
+
+    }
+
+}
